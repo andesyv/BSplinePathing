94d2926f57d55bd657f83e2e1877f625ab3902c3
andesyv <andesyv@gmail.com>
2019-11-08 18:35:09 +0100

    Optimized terraindata generation so that it only needs to generate on it's first run.
    It creates a file called terrainData.txt that it can load the next time.

--- renderwindow.h
+++ renderwindow.h
@@ -45,6 +45,10 @@ private slots:
 private:
     void init();
     void setCameraSpeed(float value);
+    void initTerrain();
+    bool readTerrainData(std::string file);
+    void writeTerrainData(std::string file);
+    void constructTerrain();
 
     QOpenGLContext *mContext{nullptr};
     bool mInitialized{false};
